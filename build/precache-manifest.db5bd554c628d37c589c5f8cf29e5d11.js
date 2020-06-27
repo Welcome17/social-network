e0a9a43083ee4a011e2e8fdc25a82b9a985a5414
John <K2023769@gmail.com>
2020-06-27 15:21:45 +0400

    Added Build folder

---
+++ build/precache-manifest.db5bd554c628d37c589c5f8cf29e5d11.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ebe2456d927596172edc1045a68717a",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "b032a675754d7d5d410e",
+    "url": "/social-network/static/css/3.cb99110c.chunk.css"
+  },
+  {
+    "revision": "00ad6c8365d851409788",
+    "url": "/social-network/static/css/main.77e22887.chunk.css"
+  },
+  {
+    "revision": "b9721979b2a98310a768",
+    "url": "/social-network/static/js/2.a5e98746.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-network/static/js/2.a5e98746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b032a675754d7d5d410e",
+    "url": "/social-network/static/js/3.50fcaf23.chunk.js"
+  },
+  {
+    "revision": "f8372d04d9358acc4a0f",
+    "url": "/social-network/static/js/4.0732a72f.chunk.js"
+  },
+  {
+    "revision": "00ad6c8365d851409788",
+    "url": "/social-network/static/js/main.4d919568.chunk.js"
+  },
+  {
+    "revision": "7a67745cfa95b7cbfc51",
+    "url": "/social-network/static/js/runtime-main.9f5b2bde.js"
+  },
+  {
+    "revision": "27fc27b682d7147fc0935299c8467922",
+    "url": "/social-network/static/media/preloader.27fc27b6.svg"
+  },
+  {
+    "revision": "b060e569e165a711971086b20862ecdb",
+    "url": "/social-network/static/media/user.b060e569.png"
+  }
+]);
